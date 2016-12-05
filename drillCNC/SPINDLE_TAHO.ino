c5b9bb7ac0c22ff5bd725717a5bd5bb75b7e2b06
musgravehill <musgravehill@gmail.com>
2016-12-05 13:42:26 +0300

    pid map

--- drillCNC/SPINDLE_TAHO.ino
+++ drillCNC/SPINDLE_TAHO.ino
@@ -9,9 +9,9 @@ void TAHO_interrupt () {
 void TAHO_calc_PRM() {
   TAHO_RPM = TAHO_rotationCount * 600; //10* 100ms * countRotation  * 60 = rot per minute
   TAHO_rotationCount = 0;
-  PID_input_rpm = TAHO_RPM;
+  PID_input_rpm = map(TAHO_RPM, 0, 30000, 0, 1024);
 }
 
 void SPINDLE_SPEED_SET() {
-  PID_setpoint_rpm = 30 * analogRead(PIN_SPINDLE_SPEED);
+  PID_setpoint_rpm = map(30 * analogRead(PIN_SPINDLE_SPEED), 0, 30000, 0, 1024);
 }
