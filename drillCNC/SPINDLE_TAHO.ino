136b7c0d8e1e342e84f89a7c8ce900e57f26d4b7
musgravehill <musgravehill@gmail.com>
2016-12-27 09:25:18 +0300

    taho

--- drillCNC/SPINDLE_TAHO.ino
+++ drillCNC/SPINDLE_TAHO.ino
@@ -8,9 +8,10 @@ void TAHO_interrupt () {
 
 void TAHO_calc_PRM() {
   //cli();
-  uint32_t  TAHO_curr_ms = millis();    
-  TAHO_RPM = TAHO_rotationCount * 30000L / (TAHO_curr_ms - TAHO_prev_ms); //rot per minute   60 000ms = 60s = 1 minute  2-holes => 60\2 = 30
-  PID_input_0_1023 = map(TAHO_RPM, 0, 8000, 0, 1023);
+  uint32_t  TAHO_curr_ms = millis();  
+  Serial.println(TAHO_rotationCount);  
+  TAHO_RPM = TAHO_rotationCount * 15000L / (TAHO_curr_ms - TAHO_prev_ms); //rot per minute   60 000ms = 60s = 1 minute  4-holes => 60\4 = 15
+  PID_input_0_1023 = map(TAHO_RPM, 0, 8300, 0, 1023);
   TAHO_rotationCount = 0L;
   TAHO_prev_ms = TAHO_curr_ms;
   //sei();
@@ -18,5 +19,5 @@ void TAHO_calc_PRM() {
 }
 
 void SPINDLE_SPEED_SET() {
-  PID_setpoint_0_1023 = 1023 - analogRead(PIN_SPINDLE_SPEED); //reostat inversed rotation
+  PID_setpoint_0_1023 = 1020 - analogRead(PIN_SPINDLE_SPEED); //reostat inversed rotation
 }
